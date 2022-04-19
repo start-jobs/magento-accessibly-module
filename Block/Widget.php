ff46e614197fc375e0f44d921f3f5fd7089afb19
Arturs Juhnevics <arturs@minimo.lv>
2022-04-19 13:03:15 +0300

    load script in frontend

---
+++ Block/Widget.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Onthemap\Accessibly\Block;
+
+use Magento\Store\Model\ScopeInterface;
+
+class Widget extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+        parent::__construct($context);
+    }
+    public function getScriptUrl()
+    {
+        $id = $this->_scopeConfig->getValue('otm_accessibly/general/api', ScopeInterface::SCOPE_STORE);
+        $url = 'https://staging.ac.onthemapmarketing.com/widget/'. $id .'/autoload.js';
+        if ($id != '') {
+            return $url;
+        }
+
+        return;
+    }
+}
